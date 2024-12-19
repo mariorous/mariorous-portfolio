664953b6969d1ee0c8ee94012ea19ea1af85b21a
〽️ario <emariors@gmail.com>
2024-12-19 12:57:21 +0100

    Add new header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
     import { ref } from 'vue'
-    import Header from './components/Header.vue'
+    import FlotationHeader from './components/FlotationHeader.vue'
     import HeroSection from './components/HeroSection.vue'
+    import Header from './components/Header.vue'
 </script>
 
 <template>
+    <!-- <FlotationHeader /> -->
     <Header />
     <HeroSection />
 </template>
