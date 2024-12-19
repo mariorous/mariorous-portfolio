bfb51a797639cc76fa25106dcd730d4e5bb145c0
〽️ario <emariors@gmail.com>
2024-12-19 12:42:50 +0100

    Header and hero section

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
-
+    import { ref } from 'vue'
+    import Header from './components/Header.vue'
+    import HeroSection from './components/HeroSection.vue'
 </script>
 
 <template>
-
+    <Header />
+    <HeroSection />
 </template>
 
 <style scoped>
---
+++ src/components/HeroSection.vue
@@ -0,0 +1,55 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="overflow-hidden">
+    <!-- Gradients -->
+    <div aria-hidden="true" class="flex absolute -top-96 start-1/2 transform -translate-x-1/2">
+        <div class="bg-gradient-to-r from-violet-300/50 to-purple-100 blur-3xl w-[25rem] h-[44rem] rotate-[-60deg] transform -translate-x-[10rem]"></div>
+        <div class="bg-gradient-to-tl from-blue-50 via-blue-100 to-blue-50 blur-3xl w-[90rem] h-[50rem] rounded-fulls origin-top-left -rotate-12 -translate-x-[15rem]"></div>
+    </div>
+    <!-- End Gradients -->
+
+    <div class="relative z-10">
+        <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10 lg:py-16">
+        <div class="max-w-2xl text-center mx-auto">
+            <p class="inline-block text-sm font-medium bg-clip-text bg-gradient-to-l from-blue-600 to-violet-500 text-transparent">
+            Portfolio version 1.0
+            </p>
+
+            <!-- Title -->
+            <div class="mt-5 max-w-2xl">
+            <h1 class="block font-semibold text-gray-800 text-4xl md:text-5xl lg:text-6xl">
+                Hello <span class="bg-clip-text bg-gradient-to-l from-violet-500 to-blue-600 text-transparent">I'm Mario López</span>
+            </h1>
+            </div>
+            <!-- End Title -->
+
+            <div class="mt-5 max-w-3xl">
+            <p class="text-lg text-gray-600">Full Stack Developer with a passion for creating innovative and efficient solutions. I thrive on building robust applications and continuously expanding my knowledge in the ever-evolving field of technology.</p>
+            </div>
+
+            <!-- Buttons -->
+            <div class="mt-8 gap-3 flex justify-center">
+            <a class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none" href="/src/assets/mario-lopez-resume.pdf" download="mario-lopez-resume.pdf">
+                Resume
+                <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
+            </a>
+            <a class="py-3 px-4 inline-flex items-center gap-x-1 text-sm font-medium rounded-lg border border-transparent text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none" href="https://github.com/mariorous">
+                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 30 30">
+                    <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
+                </svg>
+                GitHub
+            </a>
+            </div>
+            <!-- End Buttons -->
+        </div>
+        </div>
+    </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
