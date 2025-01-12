a1798a5f6be23e1b2c5bb2ecf8788625e56c0c8a
〽️ario <emariors@gmail.com>
2025-01-12 23:56:53 +0100

    Color correction on header links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -42,17 +42,17 @@ const toggleLanguage = () => {
         <div id="hs-header-classic" class="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow md:block" aria-labelledby="hs-header-classic-collapse">
         <div class="overflow-hidden overflow-y-auto max-h-[75vh] [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300">
             <div class="py-2 md:py-0 flex flex-col md:flex-row md:items-center md:justify-end gap-0.5 md:gap-1">
-            <a class="font-medium p-2 flex items-center text-sm text-gray-800 hover:text-blue-500 focus:outline-none dark:text-blue-200 dark:hover:text-blue-500" href="#" aria-current="page">
+            <a class="font-medium p-2 flex items-center text-sm text-gray-800 hover:text-blue-500 focus:outline-none dark:text-gray-200 dark:hover:text-blue-500" href="#" aria-current="page">
                 <svg class="shrink-0 size-4 me-3 md:me-2 block md:hidden" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"/><path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/></svg>
                 {{ $t('home') }}
             </a>
 
-            <a class="font-medium p-2 flex items-center text-sm text-gray-800 hover:text-blue-500 focus:outline-none dark:text-blue-200 dark:hover:text-blue-500" href="#projects">
+            <a class="font-medium p-2 flex items-center text-sm text-gray-800 hover:text-blue-500 focus:outline-none dark:text-gray-200 dark:hover:text-blue-500" href="#projects">
                 <svg class="shrink-0 size-4 me-3 md:me-2 block md:hidden" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
                 {{ $t('projects') }}
             </a>
 
-            <a class="font-medium p-2 flex items-center text-sm text-gray-800 hover:text-blue-500 focus:outline-none dark:text-blue-200 dark:hover:text-blue-500" href="#aboutMe">
+            <a class="font-medium p-2 flex items-center text-sm text-gray-800 hover:text-blue-500 focus:outline-none dark:text-gray-200 dark:hover:text-blue-500" href="#aboutMe">
                 <svg class="shrink-0 size-4 me-3 md:me-2 block md:hidden" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 12h.01"/><path d="M16 6V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v2"/><path d="M22 13a18.15 18.15 0 0 1-20 0"/><rect width="20" height="14" x="2" y="6" rx="2"/></svg>
                 {{ $t('aboutMe') }}
             </a>
